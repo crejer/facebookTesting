3bd9a58c08b5c750b122290aa3dd7b6f5afb4cdc
jjcrevit <jeroen.crevits@ugent.be>
2014-03-10 12:11:59 +0100

    facebook testing

---
+++ FacebookTesting/src/main/java/main/Login.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package main;
+
+import com.restfb.DefaultFacebookClient;
+import com.restfb.FacebookClient;
+import com.restfb.types.User;
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author jeroen
+ */
+public class Login {
+    
+	public static void main(String[] args) {
+            
+	}
+    
+}
